9550ad44f6bb946ae2b1d055b1b5fa77559f8af8
Koon Fung Yee <kelvin2274@hotmail.com>
2020-05-04 11:51:06 +0800

    4.5.20

--- lib/login.dart
+++ lib/login.dart
@@ -6,7 +6,6 @@ import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:fluttericon/typicons_icons.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:progress_indicators/progress_indicators.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -91,7 +90,7 @@ class _LoginPageState extends State<Login> {
           child: AppBar(
             brightness: Brightness.light,
             backgroundColor: Colors.white,
-            elevation: 1,
+            elevation: 0,
           ),
         ),
         body: SingleChildScrollView(
